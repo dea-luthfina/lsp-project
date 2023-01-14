0f2afcd4d41eb66cad81f9be3db143cf20e842c8
kaniraerlna <kanira.zalea@gmail.com>
2023-01-14 10:25:55 +0700

    add folder assets dan modified login.php

--- src/login.php
+++ src/login.php
@@ -71,60 +71,56 @@
 </head>
 <body>
     <div class="container">
-        <div class="forms">
-            <div class ="form register">
-                <center>Login</center>
-                <br>
-                <form action="login.php" method="post">
-                    <?php if($error != ''){ ?>
-                        <div class="alert alert-danger alert-dismissible fade show" style="margin-top: 10px;" role="alert">
-                            <?=$error; ?>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
-                            </button>
-                        </div>
-                    <?php } ?>
-                    <?php if($errorCaptcha != ''){ ?>
-                        <div class="alert alert-danger alert-dismissible fade show" style="margin-top: 10px;" role="alert">
-                            <?=$errorCaptcha; ?>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
-                            </button>
-                        </div>
-                    <?php } ?>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
-                        <input type="text" class="form-control" required name="email" id="email" autocomplete="off" placeholder="email">
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text"><i class="fa-solid fa-lock"></i></span>
-                        <input type="password" class="form-control" minlength=6 maxlength=8 required name="password" id="password" autocomplete="off" placeholder="Password">
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text">Security Code</i></span>
-                        <img src="captcha/captcha.php">
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text">Enter Security Code</i></span>
-                        <input type="text" class="form-control" required name="captchaCode" maxlength="6">
-                    </div>
-                    <div class="checkbox-text">
-                        <div class="checkbox-content">
-                            <input type="checkbox" id="logCheck">
-                            <label for="logCheck" class="text">Remember Me</label>
-                        </div>
-                        <a href="#" class="text">Forgot Password?</a>
-                    </div>
-                    <div class="d-grid gap-2 col-14 mx-auto" style="margin-top: 20px;">
-                        <button class="btn btn-primary" type="submit" name="submit">Login</button>
-                    </div>
-
-                    <div class="login-signup">
-                        <span class="text">Not a librarian?
-                            <a href="register.php" class="signup-text">Register now</a>
-                        </span>
-                    </div>
-                </form>
+        <center>Login</center>
+        <br>
+        <form action="login.php" method="post">
+            <?php if($error != ''){ ?>
+                <div class="alert alert-danger alert-dismissible fade show" style="margin-top: 10px;" role="alert">
+                    <?=$error; ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                    </button>
+                </div>
+            <?php } ?>
+            <?php if($errorCaptcha != ''){ ?>
+                <div class="alert alert-danger alert-dismissible fade show" style="margin-top: 10px;" role="alert">
+                    <?=$errorCaptcha; ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                    </button>
+                </div>
+            <?php } ?>
+            <div class="input-group mb-3">
+                <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
+                <input type="text" class="form-control" required name="email" id="email" autocomplete="off" placeholder="email">
             </div>
-        </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text"><i class="fa-solid fa-lock"></i></span>
+                <input type="password" class="form-control" minlength=6 maxlength=8 required name="password" id="password" autocomplete="off" placeholder="Password">
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text">Security Code</i></span>
+                <img src="captcha/captcha.php">
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text">Enter Security Code</i></span>
+                <input type="text" class="form-control" required name="captchaCode" maxlength="6">
+            </div>
+            <div class="checkbox-text">
+                <div class="checkbox-content">
+                    <input type="checkbox" id="logCheck">
+                    <label for="logCheck" class="text">Remember Me</label>
+                </div>
+                <a href="#" class="text">Forgot Password?</a>
+            </div>
+            <div class="d-grid gap-2 col-14 mx-auto" style="margin-top: 20px;">
+                <button class="btn btn-primary" type="submit" name="submit">Login</button>
+            </div>
+
+            <div class="login-signup">
+                <span class="text">Not a librarian?
+                    <a href="register.php" class="signup-text">Register now</a>
+                </span>
+            </div>
+        </form>
     </div>
 </body>
 </html>
